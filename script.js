3a84a70c0bee4bcd57ae9ae65b89438515f9717a
mrdjarade <radem98@gmail.com>
2022-01-02 13:40:21 +0100

    Nove funkcionalnosti

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#novi-kurs").click(function (e) {
+    e.preventDefault();
+    $(".prozor-kurs").toggleClass("hidden-kurs");
+});
+
+$("#dodajNovi").submit(function (e) {
+    e.preventDefault();
+    console.log("Dodavanje");
+})
