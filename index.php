d7db2cd50418484455067536ca49bb31ea3a5dd8
mrdjarade <radem98@gmail.com>
2022-01-02 15:48:09 +0100

    Dodatna unapredjenja

--- index.php
+++ index.php
@@ -4,10 +4,22 @@ require "broker.php";
 require "model/Kurs.php";
 
 session_start();
-if(!isset($_SESSION["user_id"])) {
+if(!isset($_SESSION['adminID'])) {
     header("Location: login.php");
     exit();
 }
+
+$svi = Kurs::prikaziSve($conn);
+
+if(!$svi) {
+    echo "Došlo je do greške.";
+    exit();
+}
+
+if($svi->num_rows == 0) {
+    echo "Nema postojećih kurseva.";
+    exit();
+} else {
 
 ?>
 
@@ -18,26 +30,28 @@ if(!isset($_SESSION["user_id"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="css/style.css">
     <title>Početna strana</title>
 </head>
 <body>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <div class="padding-container">
         <h1>Dobrodošli</h1>
         <p>Ovo je aplikacija o kursevima, koje različiti administratori mogu da kreiraju, menjaju, brišu i pretražuju.</p>
         <button style="margin-bottom:30px;" type="button" id="novi-kurs" class="btn btn-success">Novi kurs</button>
 
         <div class="prozor-kurs">
-            <form>
+            <form action="#" method="POST" id="dodajNovi">
                 <div class="form-group">
                     <label>Naziv kursa</label>
-                    <input type="text" class="form-control" placeholder="Unesite naziv kursa...">
+                    <input name="naziv" type="text" class="form-control" placeholder="Unesite naziv kursa...">
                 </div>
                 <div class="form-group">
                     <label>Kratak opis kursa</label>
-                    <textarea type="text" class="form-control" placeholder="Unesite kratak opis kursa..."></textarea>
+                    <textarea name="opis" type="text" class="form-control" placeholder="Unesite kratak opis kursa..."></textarea>
                 </div>
-                <button type="submit" class="btn btn-primary">Dodaj</button>
+                <button id="btnDodaj" formmethod="POST" type="submit" class="btn btn-primary">Dodaj</button>
             </form>
         </div>
 
@@ -50,37 +64,33 @@ if(!isset($_SESSION["user_id"])) {
                     <th scope="col">Kurs ID</th>
                     <th scope="col">Naziv kursa</th>
                     <th scope="col">Opis</th>
-                    <th scope="col">Autor</th>
+                    <th scope="col">Autor ID</th>
                     <th scope="col">Opcije</th>
                 </tr>
             </thead>
             <tbody>
+                <?php
+                    while($row = $svi->fetch_array()):
+                ?>
                 <tr>
-                    <td scope="row">1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
+                    <td scope="row"><?php echo $row["kursID"] ?></td>
+                    <td><?php echo $row["naziv"] ?></td>
+                    <td><?php echo $row["opis"] ?></td>
+                    <td><?php echo $row["autor"] ?></td>
+                    <td>
+                        <button formmethod="POST" type="button" class="btn btn-warning btn-sm"><i class="fas fa-pen"></i></button>
+                        <button formmethod="POST" type="button" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+                    </td>
                 </tr>
-                <tr>
-                    <td scope="row">2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td scope="row">3</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                </tr>
+                <?php
+                    endwhile;
+                }
+                ?>
             </tbody>
         </table>
     </div>
     
-    <script src="js/script.js"></script>
-
+    <script src="script.js"></script>
+    
 </body>
 </html>
