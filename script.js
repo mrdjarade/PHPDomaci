14843f101f332f4f3ba7e614b1f7ee83947a6de9
mrdjarade <radem98@gmail.com>
2022-01-02 15:46:21 +0100

    Ajax za dodavanje novog kursa

--- script.js
+++ script.js
@@ -6,4 +6,31 @@ $("#novi-kurs").click(function (e) {
 $("#dodajNovi").submit(function (e) {
     e.preventDefault();
     console.log("Dodavanje");
+    const $form = $(this);
+    const $input = $form.find('input, textarea, button, select');
+    const serijalizacija = $form.serialize();
+    console.log(serijalizacija);
+    $input.prop('disabled', true);
+
+    request = $.ajax({
+        url: 'controller/dodaj.php',
+        type: 'POST',
+        data: serijalizacija
+    });
+
+    request.done(function (res, textStatus, jqXHR) {
+        if (res === 'Success') {
+            alert("Novi kurs je uspešno dodat.");
+            console.log("Dodat je novi kurs");
+            location.reload(true);
+        } else {
+            console.log("Novi kurs nije dodat. " + res);
+        }
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+        console.error("Desila se greska -> " + errorThrown);
+    });
+
+
 });
