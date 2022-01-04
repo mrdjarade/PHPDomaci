eceb6cf0a06971dd264e1f58a25207f97e746b03
mrdjarade <radem98@gmail.com>
2022-01-04 16:55:42 +0100

    Izmenjen controller za azuriranje postojecih redova u bazi

--- controller/izmeni.php
+++ controller/izmeni.php
@@ -3,10 +3,13 @@
 require "../broker.php";
 require "../model/Kurs.php";
 
-if(isset($_POST["naziv"]) && isset($_POST["opis"]) && isset($_POST["id"])) {
-    $novi = new Kurs(null, $_POST["naziv"], $_POST["opis"], null);
-    $status = $novi->update($_POST["id"], $conn);
-    if($status) {
+if(isset($_POST["autor"])) {
+    $naziv = $_POST['naziv'];
+    $opis = $_POST['opis'];
+    $id = $_POST['id'];
+    
+    $result = mysqli_query($conn, "UPDATE kurs SET naziv='$naziv', opis='$opis' WHERE kursID='$id'");
+    if($result) {
         echo 'Success';
     } else {
         echo 'Failed';
